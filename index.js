f2065eaf2a91032450b1e6616793e3a6f8ddb3b0
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-06-20 11:11:45 +0300

    drum kit

--- index.js
+++ index.js
@@ -16,33 +16,7 @@ Buttons[x].addEventListener("click", function (){
 
 /**********************************************/
 
-//A Simple Calculator Formula
 
-function add (num1, num2){
-    return num1 + num2;
-}
-
-function substract (num1, num2){
-    return num1 - num2;
-}
-
-function multiply (num1, num2){
-    return num1 * num2;
-}
-
-function divide (num1, num2){
-    return num1 / num2;
-}
-
-function remainder (num1, num2){
-    return num1 % num2;
-}
-
-function calculator (num1, num2, operator) {
-    return operator(num1, num2);
-}
-
-console.log (calculator (11, 5, remainder))
 
 
 
